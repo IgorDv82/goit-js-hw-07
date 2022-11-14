aaa892903200a074efb792bccce95a0f73f50fcb
Igor Dorianskyi <dis.igor@gmail.com>
2022-11-14 23:48:36 +0200

    complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryLocation = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const listOfImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+            />
+        </a>
+    </div>`
+  )
+  .join("");
+
+galleryLocation.insertAdjacentHTML("beforeend", listOfImg);
+
+const clickOnImage = (evt) => {
+  evt.preventDefault();
+  const imageEl = evt.target;
+  const currentImgEl = imageEl.classList.contains("gallery__image");
+  if (!currentImgEl) {
+    return;
+  }
+  const imageUrl = imageEl.dataset.source;
+
+  const galleryInstance = basicLightbox.create(`<img src="${imageUrl}"/>`);
+
+  galleryInstance.show();
+
+  galleryLocation.addEventListener("keydown", (evt) => {
+    if (evt.key === "Escape") {
+      galleryInstance.close();
+    }
+  });
+};
+
+galleryLocation.addEventListener("click", clickOnImage);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryLocation = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const listOfImg = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li>
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                style="display: block"
+            />
+        </a>
+    </li>`
+  )
+  .join("");
+
+galleryLocation.insertAdjacentHTML("beforeend", listOfImg);
+
+const lighbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
